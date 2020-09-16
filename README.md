42bcf70175a4f7364c4b9a4b879fbf303958452f
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-16 15:16:48 -0700

    Added some tests, formatted

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 <img src="https://img.shields.io/coveralls/github/swordcss/gulp-sword?style=for-the-badge"><br />
 
 # gulp-sword
+
 Compile SwordCSS to CSS
 
 ## Installation
+
 `npm i gulp-sword`
 
 ### Usage
+
 Use it like you would anyother Gulp plugin.
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,6 +5,14 @@ const File = require("vinyl");
 const sword = require("../src/index");
 
 describe("gulp-sword", () => {
+  it("should return the same file", () => {
+    const fakeFile = { isNull: () => true };
+    const stream = sword();
+    stream.write(fakeFile);
+    stream.on("data", (file) => {
+      expect(file).to.equal(fakeFile);
+    });
+  });
   it("should return compiled CSS", () => {
     const fakeFile = new File({
       contents: Buffer.from(".a{sw-id:b;}#b{width:100%;}"),
